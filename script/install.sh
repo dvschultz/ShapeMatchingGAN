598b8b6be4da49b43a36f44081e1184320d8be01
Derrick Schultz <dvsmethid@gmail.com>
2020-03-18 16:23:16 -0400

    basic install script

---
+++ script/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#run bash ./script/install.sh from the root folder
+
+pip install gdown
+
+cd ShapeMatchingGAN/save
+gdown --id 1gjHR39deUSPChtRbKAD80waoQFTiXyMs #download example checkpoint files
+unzip ShapeMGAN-data_plus_model.zip
+rm -r data/style
+mkdir ../data/rawtext/yaheiB/train
+mv data/rawtext/yaheiB/train/*.* ../data/rawtext/yaheiB/train/
+mkdir ../data/rawtext/augment
+mv data/rawtext/augment/*.* ../data/rawtext/augment/
+mv save/*.* ./
+rmdir save
+rm ShapeMGAN-data_plus_model.zip
+rm -r data
+cd ../src
